9b2cec0bcadc2176089afcb90159963e9b6c5dce
Xenotransplantator <p.sawala99@wp.pl>
2021-11-20 10:38:00 +0100

    ĆwiczenieWPF

---
+++ Cw1Hello/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw1Hello
+{
+    class ProgramWorld
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Witam Jestem Piotr");
+            System.Console.WriteLine("Zadanie Wykonane!");//Commmit1
+        }
+    }
+}
